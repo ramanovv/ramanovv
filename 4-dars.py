a6c15916ed6e109920e566093fe98aa001cfed8c
ramanovv <allayarovelnur@gmail.com>
2024-10-11 09:20:03 +0300

    Create 4-dars.py

---
+++ 4-dars.py
@@ -0,0 +1,27 @@
+python = {
+    'olma':10000,
+    'anor':9000,
+    'uzum':15000,
+    'banan':13000,
+    'nok':19000,
+    'bexi':26000,
+    'gilos':30000,
+    'qorali':14000,
+    'anjir':36000,
+    'shaftoli':31000
+}
+for maxsulot, qiymat in sorted(python.items()) :
+    print(f"maxsulot: {maxsulot}")
+    print(f"qiymat: {qiymat}")
+
+bmt = {
+    'rassia':'maskva',
+    'qozog`iston':'astana',
+    'kizg`iziston':'bishkek',
+    'tojikiston':'dushanbe',
+    'turkmaniston':'ashxobod',
+    'o`zbekiston':'toshkent'
+}
+for davlat, poytaxti in sorted(bmt.items()) :
+    print(f"davlat: {davlat}")
+    print(f"poytaxti: {poytaxti}\n")
