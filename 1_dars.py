2252f4c62d020c936507c65bb355feb251604d17
ramanovv <allayarovelnur@gmail.com>
2024-10-11 09:08:58 +0300

    Create 1_dars.py

---
+++ 1_dars.py
@@ -0,0 +1,26 @@
+
+kocha = 'Bog`bon'
+mahalla = 'Sog`bon'
+tuman = 'Bodomzor'
+viloyay = 'Samarqand'
+barchasi =  f"{kocha} ko`chasi, {mahalla} mahallasi, {tuman} tumani, {viloyay} viloyati "
+
+
+a = int(input("son kiriting"))
+k = a ** 2
+q = a ** 3
+print('kvadrati ',k ,'qubi', q )
+
+
+t_yil = int (input('yoshingizni kiriting: '))
+yosh = 2024 - t_yil
+print('siz' , yosh , 'yilda tug`ulgan ekansiz')
+
+
+p = int(input('ikkita son kiriting'  ))
+l = int(input())
+a = p + l
+b = p - l
+d = p * l
+e = p / l
+print('yig`indisi ',a,'ayirmasi ',b,'ko`ytmasi ',d,'bo`linmasi',e,)
